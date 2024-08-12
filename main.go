c7f6042dd989be4b6916de808e91341bdf93056b
Mariano Gappa <spinetta@gmail.com>
2024-08-12 15:52:33 +0100

    Used Println directly to print arrow.Record.

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	"fmt"
-	"strings"
 
 	"github.com/apache/arrow/go/v17/arrow"
 	"github.com/apache/arrow/go/v17/arrow/array"
@@ -24,63 +23,11 @@ func _makeSampleArrowRecord() arrow.Record {
 	return array.NewRecord(schema, []arrow.Array{col}, int64(col.Len()))
 }
 
-// Helper function to format the value at a specific row index
-func _formatValue(col arrow.Array, rowIdx int) string {
-	switch col := col.(type) {
-	case *array.Int32:
-		return fmt.Sprintf("%d", col.Value(rowIdx))
-	case *array.Float64:
-		return fmt.Sprintf("%f", col.Value(rowIdx))
-	case *array.String:
-		return col.Value(rowIdx)
-	// Add more cases as needed for other types
-	default:
-		return "?"
-	}
-}
-
-// Print an arrow record to stdout in a Table format
-func _printArrowRecord(rec arrow.Record) {
-	schema := rec.Schema()
-	numRows := int(rec.NumRows())
-	numCols := int(rec.NumCols())
-
-	fmt.Println("----------------------------------")
-	fmt.Println("The record I'm about to print has:")
-	fmt.Println("NumRows:", numRows)
-	fmt.Println("NumCols:", numCols)
-	fmt.Println("Schema:", schema)
-	fmt.Println()
-
-	// Print the header
-	headers := make([]string, numCols)
-	for i, field := range schema.Fields() {
-		if i >= numCols {
-			break
-		}
-		headers[i] = field.Name
-	}
-	fmt.Println(strings.Join(headers, "\t"))
-
-	// Print the rows
-	for rowIdx := 0; rowIdx < numRows; rowIdx++ {
-		row := make([]string, numCols)
-		for colIdx := 0; colIdx < numCols; colIdx++ {
-			col := rec.Column(colIdx)
-			row[colIdx] = _formatValue(col, rowIdx)
-		}
-		fmt.Println(strings.Join(row, "\t"))
-	}
-	fmt.Println()
-	fmt.Println("I'm done printing the record")
-	fmt.Println("----------------------------------")
-}
-
 func main() {
 	// Create a sample arrow record
 	rec := _makeSampleArrowRecord()
 	fmt.Println("Record before running SQL:")
-	_printArrowRecord(rec)
+	fmt.Println(rec)
 
 	// Run SQL
 	ctx := context.Background()
@@ -96,6 +43,6 @@ func main() {
 
 	// Print the result records
 	for _, resultRecord := range resultRecords {
-		_printArrowRecord(resultRecord)
+		fmt.Println(resultRecord)
 	}
 }
